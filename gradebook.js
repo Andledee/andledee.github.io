fae7b5d9ec00f790ee5d2b6f96e3d3aed6192b03
Andledee <andie.meddaugh@gmail.com>
2025-07-07 17:52:55 -0400

    Add files via upload
    First draft

---
+++ gradebook.js
@@ -0,0 +1,18 @@
+//TO DO: Fetch data from the PostgreSQL database
+function fetchGradeData() {
+    //This function will query the PostgreSQL database and return grade data
+    console.log("Fetching grade data...");
+}
+
+//TODO: Populate table with grade data
+function populateGradebook(data) {
+    //Will take fetched grade data and populate table
+    console.log("Populating gradebook with data:", data);
+
+}
+
+//TODO Remove this
+//Call the stubs to demonstrate workflow
+const gradeData = fetchGradeData();
+populatedGradebook(gradeData);
+//END REMOVE
